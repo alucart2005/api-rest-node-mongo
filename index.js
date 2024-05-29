b4d0e0544e9bd8c877af21fdadc297113a4efee6
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-05-29 18:51:16 -0500

    Feature: Creating a Node.js Server

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const {conexion} = require("./basedatos/conexion")
+const { conexion } = require("./basedatos/conexion");
+const express = require("express");
+const cors = require("cors");
 
-// inicializar app
-console.log("Node application started")
+// Inicializar app
+console.log("Node application started");
 
 // Conectar base de datos
 conexion();
+
+// Crear servidor Node
+const app = express();
+const puerto = 3900;
+// Invocar middleware
+// Configurar cors
+app.use(cors());
+
+//Leer y convertir el body a objeto js
+app.use(express.json());
+
+// Crear rutas
+
+// Crear servidor y escuchar peticiones
+app.listen(puerto,()=>{
+  console.log("Server running on port "+puerto)
+});
